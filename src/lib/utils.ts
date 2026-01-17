ab20c2de6ae1509f65903e95e6b6cde2f24f4bf4
Paolo <paolo@myanimae.com>
2026-01-17 21:18:32 +0100

    link4

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function formatPrice(price: number): string {
+    return new Intl.NumberFormat('fr-FR', {
+        style: 'currency',
+        currency: 'EUR'
+    }).format(price);
+}
